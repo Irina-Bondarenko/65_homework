8427164d16e21d0df72380d931e7918e15cc7b28
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-10-30 22:41:27 +0200

    init

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import { Header } from "./components/header/Header";
+import { Catalog } from "./components/catalog/Catalog";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Catalog />
+    </div>
+  );
+}
+
+export default App;
